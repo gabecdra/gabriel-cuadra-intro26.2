ad1e1db33f97fcbb016632782685d70c8c0d34ea
gabecdra <gabe.ed.cuadra@gmail.com>
2026-05-27 23:23:54 -0700

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -24,3 +24,42 @@ for (let i = 0; i < skills.length; i++) {
     skillItem.innerText = skills[i];
     skillsList.appendChild(skillItem);
 }
+
+let messageForm = document.getElementById("leave-message");
+messageForm.addEventListener("submit", function(event) {
+    
+    event.preventDefault();
+
+    let Name = event.target.elements.usersName.value;
+    let Email = event.target.elements.usersEmail.value;
+    let Message = event.target.elements.usersMessage.value;
+
+    console.log("Name:", Name);
+    console.log("Email:", Email);
+    console.log("Message:", Message);
+
+    let messageSection = document.getElementById("messages");
+    let messageList = messageSection.querySelector("ul");
+    
+    let newMessage = document.createElement("li");
+
+    newMessage.innerHTML = "<a href='mailto:" + Email +"'>" + Name + "</a><span> wrote: " + Message + "</span>";
+
+    
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.type = "button";
+
+    removeButton.addEventListener("click", function() {
+        let entry = this.parentNode;
+        entry.remove();
+    });
+    newMessage.appendChild(removeButton);
+
+    messageList.appendChild(newMessage);
+
+    messageForm.reset();
+    
+});
+
+
